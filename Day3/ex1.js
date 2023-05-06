f6ba8a848a574c54e2848b562da7a282a2a7a083
Shamim Ahemad <111426896+Shamimahemad@users.noreply.github.com>
2023-05-07 00:19:29 +0530

    Add files via upload

---
+++ Day3/ex1.js
@@ -0,0 +1,6 @@
+console.log("Hello From JavaScript ....")
+var x=20
+var num1=9
+var num2=3
+const sum = num1 * num2;
+console.log(`Sum is : ${sum}`)
---
+++ Day3/ex3.js
@@ -0,0 +1,8 @@
+
+p=4
+console.log(p)
+
+const x=`hellow this is World of
+ humans`
+//x="hello"  //this is not allowed to change const !!
+console.log(x)
