61b1922f0a4078c85033df3a49e1e72fa4516bb0
Shamim Ahemad <111426896+Shamimahemad@users.noreply.github.com>
2023-05-19 20:00:52 +0530

    Add files via upload

---
+++ Day16/HW15/App15.js
@@ -0,0 +1,17 @@
+// import UpdateEmp from './UpadateEmp.js'
+import UpdateEmployee from './UpdateEmp2'
+import Parent from './ShowEmpMain'
+export default function App15(){
+
+
+
+
+    return(
+        <div>
+            {/* <UpdateEmp></UpdateEmp> */}
+            {/* <UpdateEmployee></UpdateEmployee> */}
+            <Parent></Parent>
+        </div>
+    )
+
+}
---
+++ Day16/HW15/classLifeCycle.js
@@ -0,0 +1,12 @@
+import React from "react"
+export default class LifeCycle extent React.Component{
+
+    constructor(props){
+        super(pros)
+    }
+    
+
+    reder(){
+        return<div>The LifeCycle Component {this.props.msg}</div>
+    }
+}
